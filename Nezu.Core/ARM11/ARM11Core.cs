75c1caf0a1570d5322a0b42dbcd129cd612a955a
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-02-17 02:21:45 -0600

    Add memory to ARM11Core

--- Nezu.Core/ARM11/ARM11Core.cs
+++ Nezu.Core/ARM11/ARM11Core.cs
@@ -6,12 +6,30 @@ using System.Threading.Tasks;
 
 namespace Nezu.Core.ARM11
 {
-    public partial class ARM11Core
+    public partial class ARM11Core : IDisposable
     {
         public RegisterSet Registers = new();
+        public Memory.Memory Memory = new(512);
 
         public ARM11Core()
         {
+        }
+
+        private void Step()
+        {
+            // Fetch
+            var instruction = Memory.ReadWord(Registers[15]);
+
+            // Decode & Execute
+            ExecuteInstruction(instruction);
+
+            // Increment PC
+            ++Registers[15];
+        }
+
+        public void Dispose()
+        {
+            Memory?.Dispose();
         }
     }
 }
