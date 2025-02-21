1be82080dee56b0f04423151426353f2358929d4
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2025-02-21 14:19:24 -0500

    [WIP] Start LDM

---
+++ Nezu.Core/ARM11/Instructions/ARMv6/LoadStore.cs
@@ -0,0 +1,13 @@
+﻿namespace Nezu.Core.ARM11
+{
+    public partial class ARM11Core
+    {
+        private void ARM_LDM(uint instruction)
+        {
+            // TODO: finish after class
+            ushort regList = (ushort)(instruction);
+            bool r15 = (regList & 0x8000) != 0;
+            uint rn = (instruction >> 16) & 0xF;
+        }
+    }
+}
